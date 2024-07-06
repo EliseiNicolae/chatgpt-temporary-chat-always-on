c5b8ea00ff70eed3771f27fda5f574c8422867ff
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-07-06 17:25:43 +0300

    v1.1 - add the toggle btn

---
+++ README.md
@@ -0,0 +1,21 @@
+<p align="center">
+    <img src="assets/chatgpt_always_temporary_chat_on.png" alt="Radio Crestin logo" width="900" style="border-radius: 40px" />
+</p>
+<h2 align="center" style="height: 23px">ChatGPT Chrome Extension</h2>
+<h3 align="center">to keep enabled <b>Temporary Chat</b></h3>
+<br>
+<p align="center">
+  <a href="https://chromewebstore.google.com/detail/chatgpt-keep-temporary-ch/hgbjeckjcopbneknolackcedoknnkjpe" target="_blank">
+    <img alt="playstore image" src="assets/btn_get_extension.png" width="240"/>
+  </a>
+</p>
+
+
+
+### For development.
+
+- to create the zip, execute:
+```
+cd src
+zip -r dist.zip *
+```
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,25 +1,52 @@
+// [START] ********************** Utils **********************
+
 // Function to check and update the URL and reload the page if needed
-function updateURL () {
+function setTemporaryChatOn () {
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
 
-  // Check if the 'temporary-chat' parameter is not present
   if (!params.has('temporary-chat')) {
     // Add the 'temporary-chat=true' parameter
     params.append('temporary-chat', 'true');
     // Update the URL including the path and parameters
-    window.location.href = `${url.origin}${url.pathname}?${params}`;
+    window.location.href = `${url.origin}/?${params}`;
   }
 }
 
-// Update the URL when the script is loaded
-updateURL();
+function setTemporaryChatOff () {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+
+  // Check if the 'temporary-chat' parameter is present
+  if (params.has('temporary-chat')) {
+    // Remove the 'temporary-chat' parameter
+    params.delete('temporary-chat');
+    // Update the URL including the path and parameters
+    window.location.href = `${url.origin}/?${params}`;
+  }
+}
+
+// Function to handle the initial setting of localStorage and URL
+function handleInitialLoad () {
+  if (localStorage.getItem('temporary_chat') === null) {
+    localStorage.setItem('temporary_chat', 'true');
+    setTemporaryChatOn();
+  } else if (localStorage.getItem('temporary_chat') === 'true') {
+    setTemporaryChatOn();
+  }
+}
 
 // Handle the 'New chat' button specifically
 function handleButtonClick () {
-  setTimeout(updateURL, 2);
+  if (localStorage.getItem('temporary_chat') === 'true') {
+    setTimeout(setTemporaryChatOn, 2);
+  }
 }
 
+// [END] ********************** Utils **********************
+
+
+// [START] ********************** Observers **********************
 // Observe changes in the document to reattach event listeners to new buttons
 const observer = new MutationObserver(() => {
   document.querySelectorAll('button').forEach(button => {
@@ -31,3 +58,77 @@ observer.observe(document.body, {
   childList: true,
   subtree: true
 });
+
+// Function to check URL changes and log them
+function checkUrlChange () {
+  let previousUrl = window.location.href;
+
+  const observer = new MutationObserver((mutations, observer) => {
+    const currentUrl = window.location.href;
+    if (previousUrl !== currentUrl) {
+      previousUrl = currentUrl;
+
+      const url = new URL(window.location.href);
+      const params = new URLSearchParams(url.search);
+      if (localStorage.getItem('temporary_chat') === 'true' && !params.has('temporary-chat')) {
+        localStorage.setItem('temporary_chat', 'true');
+        const toggleButton = document.getElementById('toggle-button');
+        toggleButton.checked = true;
+        params.append('temporary-chat', 'true');
+        window.location.href = `${url.origin}/?${params}`;
+      }
+
+      // check if temporary-chat param has value 'true' and set the toggle button to true and update the local storage
+      if (params.has('temporary-chat') && params.get('temporary-chat') === 'true') {
+        const toggleButton = document.getElementById('toggle-button');
+        toggleButton.checked = true;
+        localStorage.setItem('temporary_chat', 'true');
+      }
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['href']
+  });
+}
+
+// [END] ********************** Observers **********************
+
+// Function to inject HTML content from toggle.html
+function injectHTML () {
+  fetch(chrome.runtime.getURL('toggle.html'))
+      .then(response => response.text())
+      .then(data => {
+        const div = document.createElement('div');
+        div.innerHTML = data;
+        document.body.appendChild(div);
+        const toggleButton = document.getElementById('toggle-button');
+        if (localStorage.getItem('temporary_chat') === 'true') {
+          toggleButton.checked = true;
+        } else {
+          toggleButton.checked = false;
+        }
+        toggleButton.addEventListener('change', function () {
+          if (this.checked) {
+            localStorage.setItem('temporary_chat', 'true');
+            setTemporaryChatOn();
+          } else {
+            localStorage.setItem('temporary_chat', 'false');
+            setTemporaryChatOff()
+          }
+        });
+      })
+      .catch(error => console.error('Error fetching the HTML:', error));
+}
+
+// Initial load handling
+handleInitialLoad();
+
+// Start checking for URL changes
+checkUrlChange();
+
+// Inject HTML when the script is loaded
+injectHTML();
