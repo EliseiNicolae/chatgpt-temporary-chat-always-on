d63a671edf8ab4a7e0a112ac80be7baec10a2c3d
EliseiFlixier <nicolae.elisei1999@gmail.com>
2024-07-14 15:09:23 +0300

    v1.3.1 - add popup when clicking to extension & fix toggle issues

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,47 +2,66 @@
 
 // Function to check and update the URL and reload the page if needed
 function setTemporaryChatOn () {
-  const url = new URL(window.location.href);
-  const params = new URLSearchParams(url.search);
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
 
-  if (!params.has('temporary-chat')) {
-    // Add the 'temporary-chat=true' parameter
-    params.append('temporary-chat', 'true');
+  if (!params.has('temporary-chat') || params.get('temporary-chat') === 'false') {
+    params.delete('temporary-chat')
+    params.set('temporary-chat', 'true')
     // Update the URL including the path and parameters
-    if (url.pathname === '/') {
-      window.location.href = `${url.origin}/?${params}`;
+    if (isOnChatPage) {
+      window.location.href = `${url.origin}/?${params}`
     }
   }
 }
 
 function setTemporaryChatOff () {
-  const url = new URL(window.location.href);
-  const params = new URLSearchParams(url.search);
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
 
   // Check if the 'temporary-chat' parameter is present
-  if (params.has('temporary-chat')) {
+  if (params.has('temporary-chat') || params.get('temporary-chat') === 'true') {
     // Remove the 'temporary-chat' parameter
-    params.delete('temporary-chat');
+    params.delete('temporary-chat')
     // Update the URL including the path and parameters
-    if (url.pathname === '/') {
-      window.location.href = `${url.origin}/?${params}`;
+    if (isOnChatPage) {
+      window.location.href = `${url.origin}/?${params}`
     }
   }
 }
 
 // Function to handle the initial setting of localStorage and URL
 function handleInitialLoad () {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
   if (localStorage.getItem('temporary_chat') === null) {
-    setTemporaryChatOn();
-  } else if (localStorage.getItem('temporary_chat') === 'true') {
-    setTemporaryChatOn();
+    localStorage.setItem('temporary_chat', 'true')
+    setTemporaryChatOn()
+    return
+  } else if (localStorage.getItem('temporary_chat') === 'true' && params.get('temporary-chat') !== 'false') {
+    setTemporaryChatOn()
+    return
+  }
+  if (params.has('temporary-chat') && params.get('temporary-chat') === 'true') {
+    setTimeout(() => {
+      localStorage.setItem('temporary_chat', 'true')
+      const toggleButton = document.getElementById('toggle-button')
+      toggleButton.checked = true
+    }, 100)
+  } else if (params.has('temporary-chat') && params.get('temporary-chat') === 'false') {
+    setTimeout(() => {
+      localStorage.setItem('temporary_chat', 'false')
+      const toggleButton = document.getElementById('toggle-button')
+      toggleButton.checked = false
+    }, 100)
   }
 }
 
 // Handle the 'New chat' button specifically
 function handleButtonClick () {
   if (localStorage.getItem('temporary_chat') === 'true') {
-    setTimeout(setTemporaryChatOn, 2);
+    setTimeout(setTemporaryChatOn, 2)
   }
 }
 
@@ -53,52 +72,53 @@ function handleButtonClick () {
 // Observe changes in the document to reattach event listeners to new buttons
 const observer = new MutationObserver(() => {
   document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', handleButtonClick);
-  });
-});
+    button.addEventListener('click', handleButtonClick)
+  })
+})
 
 observer.observe(document.body, {
   childList: true,
   subtree: true
-});
+})
 
 // Function to check URL changes and log them
 function checkUrlChange () {
-  let previousUrl = window.location.href;
+  let previousUrl = window.location.href
 
   const observer = new MutationObserver((mutations, observer) => {
-    const currentUrl = window.location.href;
+    const currentUrl = window.location.href
     if (previousUrl !== currentUrl) {
-      previousUrl = currentUrl;
+      previousUrl = currentUrl
 
-      const url = new URL(window.location.href);
-      const params = new URLSearchParams(url.search);
+      const url = new URL(window.location.href)
+      const params = new URLSearchParams(url.search)
+
       if (localStorage.getItem('temporary_chat') === 'true' && !params.has('temporary-chat')) {
-        localStorage.setItem('temporary_chat', 'true');
-        const toggleButton = document.getElementById('toggle-button');
-        if (toggleButton && toggleButton?.checked) {
-          toggleButton.checked = true;
+        localStorage.setItem('temporary_chat', 'true')
+        const toggleButton = document.getElementById('toggle-button')
+        if (toggleButton) {
+          toggleButton.checked = true
         }
-        setTemporaryChatOn();
+        setTemporaryChatOn()
       }
 
       // check if temporary-chat param has value 'true' and set the toggle button to true and update the local storage
       if (params.has('temporary-chat') && params.get('temporary-chat') === 'true') {
-        const toggleButton = document.getElementById('toggle-button');
-        if (toggleButton && toggleButton?.checked) {
-          toggleButton.checked = true;
+        const toggleButton = document.getElementById('toggle-button')
+        if (toggleButton) {
+          toggleButton.checked = true
         }
-        localStorage.setItem('temporary_chat', 'true');
+        localStorage.setItem('temporary_chat', 'true')
       }
     }
-  });
+  })
 
   observer.observe(document.body, {
     childList: true,
     subtree: true,
     attributes: true,
     attributeFilter: ['href']
-  });
+  })
 }
 
 // [END] ********************** Observers **********************
@@ -108,31 +128,43 @@ function injectHTMLToggle () {
   fetch(chrome.runtime.getURL('toggle.html'))
       .then(response => response.text())
       .then(data => {
-        const div = document.createElement('div');
-        div.innerHTML = data;
-        document.body.appendChild(div);
-        const toggleButton = document.getElementById('toggle-button');
-        toggleButton.checked = localStorage.getItem('temporary_chat') === 'true';
+        const div = document.createElement('div')
+        div.innerHTML = data
+        document.body.appendChild(div)
+        const url = new URL(window.location.href)
+        const params = new URLSearchParams(url.search)
+
+        const toggleButton = document.getElementById('toggle-button')
+        toggleButton.checked = localStorage.getItem('temporary_chat') === 'true'
         toggleButton.addEventListener('change', function () {
           if (this.checked) {
-            localStorage.setItem('temporary_chat', 'true');
-            setTemporaryChatOn();
+            if (params.has('temporary-chat') && params.get('temporary-chat') === 'false') {
+              params.set('temporary-chat', 'true')
+              window.location.href = `${url.origin}/?${params}`
+              return
+            }
+            localStorage.setItem('temporary_chat', 'true')
+            setTemporaryChatOn()
           } else {
-            localStorage.setItem('temporary_chat', 'false');
-            setTemporaryChatOff();
+            localStorage.setItem('temporary_chat', 'false')
+            setTemporaryChatOff()
           }
-        });
+        })
       })
-      .catch(error => console.error('Error fetching the HTML:', error));
+      .catch(error => console.error('Error fetching the HTML:', error))
 }
 
-if (window.location.pathname === '/' || window.location.pathname === '') {
+const isOnChatPage = window.location.pathname === '/' ||
+    window.location.pathname === '' ||
+    /\/c\//.test(window.location.pathname)
+
+if (isOnChatPage) {
 // Initial load handling
-  handleInitialLoad();
+  handleInitialLoad()
 
 // Start checking for URL changes
-  checkUrlChange();
+  checkUrlChange()
 
 // Inject HTML when the script is loaded
-  injectHTMLToggle();
+  injectHTMLToggle()
 }
--- src/onboarding.js
+++ src/onboarding.js
@@ -3,41 +3,60 @@ function showOnboarding () {
   fetch(chrome.runtime.getURL('onboarding.html'))
       .then(response => response.text())
       .then(data => {
-        const div = document.createElement('div');
-        div.innerHTML = data;
-        document.body.appendChild(div);
+        const div = document.createElement('div')
+        div.innerHTML = data
+        document.body.appendChild(div)
 
-        const toggle_img_element = document.getElementById('toggle-onboarding-image');
-        toggle_img_element.src = chrome.runtime.getURL('icons/toggle_onboarding.png');
+        const toggle_img_element = document.getElementById('toggle-onboarding-image')
+        toggle_img_element.src = chrome.runtime.getURL('icons/toggle_onboarding.png')
 
-        const new_chat_onboarding = document.getElementById('new-chat-onboarding');
-        new_chat_onboarding.src = chrome.runtime.getURL('icons/new_chat_onboarding.png');
+        const new_chat_onboarding = document.getElementById('new-chat-onboarding')
+        new_chat_onboarding.src = chrome.runtime.getURL('icons/new_chat_onboarding.png')
 
-        const step1 = document.getElementById('step-1');
-        const step2 = document.getElementById('step-2');
-        const nextButton = document.getElementById('next-button');
-        const prevButton = document.getElementById('prev-button');
-        const continueButton = document.getElementById('continue-button');
+        const step1 = document.getElementById('step-1')
+        const step2 = document.getElementById('step-2')
+        const nextButton = document.getElementById('next-button')
+        const prevButton = document.getElementById('prev-button')
+        const continueButton = document.getElementById('continue-button')
 
         nextButton.addEventListener('click', () => {
-          step1.classList.remove('active');
-          step2.classList.add('active');
-        });
+          step1.classList.remove('active')
+          step2.classList.add('active')
+        })
 
         prevButton.addEventListener('click', () => {
-          step2.classList.remove('active');
-          step1.classList.add('active');
-        });
+          step2.classList.remove('active')
+          step1.classList.add('active')
+        })
 
         continueButton.addEventListener('click', () => {
-          setTemporaryChatOn();
-          document.body.removeChild(div);
-        });
+          setTemporaryChatOn()
+          document.body.removeChild(div)
+          document.removeEventListener('keydown', escKeyListener)
+        })
+
+        function escKeyListener (event) {
+          if (event.key === 'Escape' || event.key === 'Esc') {
+            document.body.removeChild(div)
+            document.removeEventListener('keydown', escKeyListener)
+          }
+        }
+
+        document.addEventListener('keydown', escKeyListener)
       })
-      .catch(error => console.error('Error fetching the onboarding HTML:', error));
+      .catch(error => console.error('Error fetching the onboarding HTML:', error))
 }
 
-if ((window.location.pathname === '/' || window.location.pathname === '') && localStorage.getItem('temporary_chat') === null) {
-  localStorage.setItem('temporary_chat', 'true');
-  showOnboarding();
+const isCorrectPath = window.location.pathname === '/' ||
+    window.location.pathname === '' ||
+    /\/c\//.test(window.location.pathname)
+const url = new URL(window.location.href)
+const params = new URLSearchParams(url.search)
+
+if (isCorrectPath &&
+    localStorage.getItem('gpt_onboardingDone_chrExt') === null &&
+    (localStorage.getItem('temporary_chat') === 'true' ||
+        params.has('temporary-chat') && params.get('temporary-chat') === 'true')) {
+  localStorage.setItem('gpt_onboardingDone_chrExt', 'true')
+  showOnboarding()
 }
